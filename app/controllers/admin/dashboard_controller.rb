20d8bb02404c9dfc5b500a54ec5d275d9d1eed01
Emma Grannis <grannis.emma@gmail.com>
2022-06-06 18:04:24 +0000

    Add bare-bones display to Admin Dashboard view populating how many products and categories are in the database

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,16 +2,9 @@ class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
 
-    #user authenticiation with creds specified in .env
-  # Rails.configuration.authenticate = {
-  #   :username_key => ENV['ADMIN_USERNAME_KEY'],
-  #   :password_key => ENV['ADMIN_PASSWORD_KEY']
-  # }
-
-  # http_basic_authenticate_with name: Rails.configuration.authenticate[:username_key], password: Rails.configuration.authenticate[:password_key]
-  # # protect_from_forgery with: :exception
-
   def show
+    @product_count = Product.count(:id)
+    @category_count = Category.count(:id)
   end
 end
 
